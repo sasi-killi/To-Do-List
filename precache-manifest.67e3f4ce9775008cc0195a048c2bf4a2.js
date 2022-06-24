03905c2d9c8744e12d7bdcb8a1f645eff173c310
Sasi-Killi <killingsasi1997@gmail.com>
2022-06-24 16:21:34 +0530

    Updates

---
+++ precache-manifest.67e3f4ce9775008cc0195a048c2bf4a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a11e402c417fb61fa32",
+    "url": "/To-Do-List/static/css/main.aa7f44a2.chunk.css"
+  },
+  {
+    "revision": "5a11e402c417fb61fa32",
+    "url": "/To-Do-List/static/js/main.5a11e402.chunk.js"
+  },
+  {
+    "revision": "c15252671b20fddd1e0a",
+    "url": "/To-Do-List/static/js/1.c1525267.chunk.js"
+  },
+  {
+    "revision": "28cd4408fba51811559c",
+    "url": "/To-Do-List/static/js/runtime~main.28cd4408.js"
+  },
+  {
+    "revision": "4e9bebf3b61724eba0b7475beb708966",
+    "url": "/To-Do-List/index.html"
+  }
+];

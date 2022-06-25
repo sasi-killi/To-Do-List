53d505770f3c7928e2bbc4d536d766d0b2346652
Sasi-Killi <killingsasi1997@gmail.com>
2022-06-25 14:05:34 +0530

    initialstyles

--- src/List.js
+++ src/List.js
@@ -9,14 +9,16 @@ class List extends Component {
     const handleDelete = this.props.handleDelete;
     if (toggle) {
       todoitems = todoitems.filter((item) => {
-        return item.text.toLowerCase().indexOf(searchValue) !== -1;
+        return (
+          item.text.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
+        );
       });
     }
     const listitmes = todoitems.map((item) => (
       <div className="list-container" key={item.id} data-key={item.id}>
         <span className="list">
           <p>{item.text}</p>
-          <p>{item.time}</p>
+          <p>created on {item.time}</p>
         </span>
         <Done />
         <button className="remove" onClick={(e) => handleDelete(e)}>
